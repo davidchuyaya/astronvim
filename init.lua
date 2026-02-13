d106e7976e19e1a36fddb176c09beec535b645a0
David Chu <chudc@amazon.com>
2026-02-12 16:45:06 -0800

    Init

---
+++ init.lua
@@ -0,0 +1,48 @@
+-- This file simply bootstraps the installation of Lazy.nvim and then calls other files for execution
+-- This file doesn't necessarily need to be touched, BE CAUTIOUS editing this file and proceed at your own risk.
+local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
+  -- stylua: ignore
+  local result = vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  if vim.v.shell_error ~= 0 then
+    -- stylua: ignore
+    vim.api.nvim_echo({ { ("Error cloning lazy.nvim:\n%s\n"):format(result), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
+    vim.fn.getchar()
+    vim.cmd.quit()
+  end
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+-- validate that lazy is available
+if not pcall(require, "lazy") then
+  -- stylua: ignore
+  vim.api.nvim_echo({ { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
+  vim.fn.getchar()
+  vim.cmd.quit()
+end
+
+require "lazy_setup"
+require "polish"
+
+function _G.set_terminal_keymaps()
+  local opts = {buffer = 0}
+  
+  -- Maps <Esc> to exit terminal mode and enter normal mode
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  
+  -- (Optional) Map 'jk' to exit terminal mode as well
+  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+  
+  -- (Optional) Easily navigate out of the terminal window into other splits
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+end
+
+-- The pattern term://*toggleterm#* ensures these mappings 
+-- only apply to toggleterm buffers, not all terminals.
+vim.cmd('autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()')
