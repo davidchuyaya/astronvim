8e8ed7cb14fbf2d018cbe67f950d4fe33256fafa
David Chu <chudc@amazon.com>
2026-02-13 18:47:15 +0000

    Fix remaining errors in cloud desktop

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,24 +1,10 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+local is_cloud_desktop = vim.env.NVIM_ENV == "cloud"
 
--- Customize None-ls sources
-
----@type LazySpec
 return {
   "nvimtools/none-ls.nvim",
   opts = function(_, opts)
-    -- opts variable is the default configuration table for the setup function call
-    -- local null_ls = require "null-ls"
-
-    -- Check supported formatters and linters
-    -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-    -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-
-    -- Only insert new sources, do not replace the existing ones
-    -- (If you wish to replace, use `opts.sources = {}` instead of the `list_insert_unique` function)
-    opts.sources = require("astrocore").list_insert_unique(opts.sources, {
-      -- Set a formatter
-      -- null_ls.builtins.formatting.stylua,
-      -- null_ls.builtins.formatting.prettier,
-    })
+    if is_cloud_desktop then
+      opts.sources = vim.tbl_filter(function(source) return source.name ~= "selene" end, opts.sources or {})
+    end
   end,
 }
