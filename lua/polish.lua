8c5f3e519432346bf7ab1278e7bb472e171c33e4
David Chu <chudc@amazon.com>
2026-02-23 17:50:17 +0000

    allow remote copypaste

--- lua/polish.lua
+++ lua/polish.lua
@@ -8,3 +8,17 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.formatoptions:remove("c")
   end,
 })
+
+-- OSC 52: yank to local clipboard over SSH
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
+vim.opt.clipboard = "unnamedplus"
