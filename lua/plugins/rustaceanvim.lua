66bd5a03b071871207f3220ee1f8630960719669
David Chu <chudc@amazon.com>
2026-02-24 12:36:11 -0800

    Allow running rust tests with reserved bash characters

---
+++ lua/plugins/rustaceanvim.lua
@@ -0,0 +1,14 @@
+return {
+  "mrcjkb/rustaceanvim",
+  opts = {
+    tools = {
+      -- Allow running tests with reserved bash characters (like <Type>)
+      test_executor = {
+        execute_command = function(cmd, args, cwd, opts)
+          local escaped_args = vim.tbl_map(vim.fn.shellescape, args)
+          require("rustaceanvim.executors.termopen").execute_command(cmd, escaped_args, cwd, opts)
+        end,
+      },
+    },
+  },
+}
